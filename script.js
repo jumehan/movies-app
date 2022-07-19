c7917fa6863755dac67be139dbf8883462dc1aa2
Julie Han <juliemenghan@gmail.com>
2022-07-19 12:55:05 -0400

    final

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
-// add event listener for submit button
-// get values of each input
-// append to DOM
-// add a delete button to delete line items
+/**
+ * Adds movie name and rating input and displays it in the HTML
+ * List elements (movie +rating) can be deleted with a Delete button
+ */
 
 $("#submit").on("click", function (e) {
   e.preventDefault();
@@ -11,18 +11,39 @@ $("#submit").on("click", function (e) {
   const $movieName = $(".title").val();
   const $movieRating = $(".rating").val();
 
-  const deleteButton = $("<button/>")
-                          .text("delete")
-                          .on("click", deleteList)
+  // if (!checkRating($movieRating)) {
+  //   alert("Rating should be between 0 and 10");
+  // }
 
-  $("ul").append($("<li>"));
+  // if (!checkTitle($movieName)) {
+  //   alert("Please enter a valid movie title of over 2 characters");
+  // }
 
-  $("li:last-child")
+  $("ul").append($("<li>")); //add new list element
+
+  const deleteButton = $("<button/>").text("delete");
+
+  $("li:last-child") //add movie name and rating to list created
     .text(`Title: ${$movieName} Rating: ${$movieRating}`)
-    .append(deleteButton);
+    .append(deleteButton); //adds delete button
+
+  $("ul").on("click", "li", deleteList); //deletes content on click
 
 });
 
-function deleteList() {
-  $("li").remove();
+/** Deletes the entire list (movie + rating) the delete button targets */
+function deleteList(e) {
+  $(e.target).parent().remove();
 }
+
+// function checkRating(rating) {
+//   if (Number(rating) < 0 || Number(rating) > 10) {
+//     return false;
+//   }
+// }
+
+// function checkTitle(title) {
+//   if (title.length < 2) {
+//     return false;
+//   }
+// }
